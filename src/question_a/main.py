d10f6e83051cf983420830ac632899ce4c5aa8cc
Dequan-Farooq <dequan.farooq@g.austincc.edu>
2025-03-12 04:42:47 +0000

    Question 1

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,12 @@
-#add import
+import question_a
+
+def main():
+    num = 50  
+    question_a.use_local_variable(num)  
+    
+    
+    print("Value of num in main program:", num)  
+
+
+main() 
+question_a.test_use_local_variable() 
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,11 @@
-#write functions here, don't add input('') statements here!
-def test_config():
-    return True
+def use_local_variable(num):
+    num = 10  # Local variable inside the function, doesn't affect the outer variable
+
+
+def test_use_local_variable():
+    num = 100  # Variable outside the function
+    use_local_variable(num)  # Pass the variable to the function
+
+    print("Value of num in the test case:", num)  # Expected output: 100
+    
+
