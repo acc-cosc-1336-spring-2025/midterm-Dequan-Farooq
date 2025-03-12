627ba17c425033029c6777a35bc79e3de9a10532
Dequan-Farooq <dequan.farooq@g.austincc.edu>
2025-03-12 03:00:16 +0000

    Question 2

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,26 @@
 #add import
+import question_b
+
+def main():
+    while True:
+        
+        user_input = input("Enter a number to check if it's prime (or type 'quit' to exit): ")
+
+        
+        if user_input.lower() == 'quit':
+            print("Goodbye!")
+            break
+
+       
+        if user_input.isdigit():
+            num = int(user_input)
+            
+            if question_b.is_prime(num):
+                print(f"{num} is a prime number.")
+            else:
+                print(f"{num} is not a prime number.")
+        else:
+            print("Please enter a valid number.")
+
+if __name__ == "__main__":
+    main()
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,12 @@
 #write functions here, don't add input('') statements here!
+
+def is_prime(q):
+    if q <= 1:
+        return False
+    for i in range(2, int(q ** 0.5) + 1):
+        if q % i == 0:
+            return False
+    return True  
+print(is_prime(4))
+print(is_prime(5))  
+print(is_prime(11))
