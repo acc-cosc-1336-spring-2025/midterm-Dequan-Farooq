11b39b876efac24afdb481ddaf6d095bf3100132
Dequan-Farooq <dequan.farooq@g.austincc.edu>
2025-03-12 04:54:25 +0000

    Question 3

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,14 @@
 #add import
+import question_c  
+
+def main():
+    global global_num 
+    print("Before use_global, global_num =", question_c.global_num)  
+    
+    question_c.use_global()  
+    
+    
+    print("After use_global, global_num =", question_c.global_num)  
+
+main()  
+question_c.test_use_global()
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,16 @@
-#write functions here, don't add input('') statements here!
+
+global_num = 10  
+
+
+def use_global():
+    global global_num 
+    global_num = 20  
+
+
+def test_use_global():
+    global global_num  
+    use_global()  
+
+    
+    assert global_num == 20, f"Test failed! global_num should be 20 but got {global_num}"
+    print("Test passed: global_num is now", global_num)  
